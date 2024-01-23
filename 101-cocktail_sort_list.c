c33b3aa63812b217dfb334dd299befe3dbafed53
Lucy Ouma <akinyilucy09@.com>
2024-01-22 23:30:31 -0800

    Cocktail shaker sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -16,9 +16,9 @@ void cocktail_sort_list(listint_t **list);
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
 {
 	listint_t *tmp = (*shaker)->next;
-	
+
 	if ((*shaker)->prev != NULL)
-        (*shaker)->prev->next = tmp;
+	(*shaker)->prev->next = tmp;
 	else
 	*list = tmp;
 	tmp->prev = (*shaker)->prev;
@@ -26,7 +26,7 @@ void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
 	if (tmp->next != NULL)
 	tmp->next->prev = *shaker;
 	else
-        *tail = *shaker;
+	*tail = *shaker;
 	(*shaker)->prev = tmp;
 	tmp->next = *shaker;
 	*shaker = tmp;
@@ -42,7 +42,7 @@ void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
 {
 	listint_t *tmp = (*shaker)->prev;
-	
+
 	if ((*shaker)->next != NULL)
 	(*shaker)->next->prev = tmp;
 	else
@@ -67,34 +67,34 @@ void cocktail_sort_list(listint_t **list)
 {
 	listint_t *tail, *shaker;
 	bool shaken_not_stirred = false;
-	
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 	return;
-	
+
 	for (tail = *list; tail->next != NULL;)
 	tail = tail->next;
-	
+
 	while (shaken_not_stirred == false)
 	{
 	shaken_not_stirred = true;
-        for (shaker = *list; shaker != tail; shaker = shaker->next)
-        {
-            if (shaker->n > shaker->next->n)
-            {
-                swap_node_ahead(list, &tail, &shaker);
-                print_list((const listint_t *)*list);
-                shaken_not_stirred = false;
-            }
-        }
-        for (shaker = shaker->prev; shaker != *list;
-                shaker = shaker->prev)
-        {
-            if (shaker->n < shaker->prev->n)
-            {
-                swap_node_behind(list, &tail, &shaker);
-                print_list((const listint_t *)*list);
-                shaken_not_stirred = false;
-            }
-        }
-    }
+	for (shaker = *list; shaker != tail; shaker = shaker->next)
+	{
+		if (shaker->n > shaker->next->n)
+		{
+		swap_node_ahead(list, &tail, &shaker);
+		print_list((const listint_t *)*list);
+		shaken_not_stirred = false;
+		}
+	}
+	for (shaker = shaker->prev; shaker != *list;
+	shaker = shaker->prev)
+	{
+	if (shaker->n < shaker->prev->n)
+	{
+	swap_node_behind(list, &tail, &shaker);
+	print_list((const listint_t *)*list);
+	shaken_not_stirred = false;
+	}
+	}
+	}
 }
